3a83d57950c05db278ec59f1ae8fbde24eb3520d
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-02 14:46:06 +0100

    Update c13-06-autoscaling-ttsp.tf

--- 17-AWS-CloudWatch/terraform-manifests/c13-06-autoscaling-ttsp.tf
+++ 17-AWS-CloudWatch/terraform-manifests/c13-06-autoscaling-ttsp.tf
@@ -5,7 +5,9 @@ resource "aws_autoscaling_policy" "avg_cpu_policy_greater_than_xx" {
   name                   = "avg-cpu-policy-greater-than-xx"
   policy_type = "TargetTrackingScaling" # Important Note: The policy type, either "SimpleScaling", "StepScaling" or "TargetTrackingScaling". If this value isn't provided, AWS will default to "SimpleScaling."    
   autoscaling_group_name = aws_autoscaling_group.my_asg.id 
-  estimated_instance_warmup = 180 # defaults to ASG default cooldown 300 seconds if not set
+  estimated_instance_warmup = 180 
+  depends_on = [ aws_autoscaling_group.my_asg, module.alb ]
+                                                  # defaults to ASG default cooldown 300 seconds if not set
   # CPU Utilization is above 50
   target_tracking_configuration {
     predefined_metric_specification {
