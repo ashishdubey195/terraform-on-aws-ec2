4d33b27012fe3f1b7d51d7bc95b62ee13ae6041c
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-08 11:01:04 +0100

    Update c4-02-vpc-module.tf

--- 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c4-02-vpc-module.tf
+++ 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c4-02-vpc-module.tf
@@ -3,7 +3,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   #version = "2.78.0"
   #version = "3.0.0"
-  version = "5.4.0"  
+  version = "6.0.1"  
 
   # VPC Basic Details
   name = "${local.name}-${var.vpc_name}"
