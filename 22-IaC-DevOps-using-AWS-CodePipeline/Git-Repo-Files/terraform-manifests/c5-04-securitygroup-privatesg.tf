2c0f961ebecdbfc7d24273fa22d52f075497c312
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-08 11:01:44 +0100

    Update c5-04-securitygroup-privatesg.tf

--- 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c5-04-securitygroup-privatesg.tf
+++ 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c5-04-securitygroup-privatesg.tf
@@ -4,7 +4,7 @@ module "private_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   #version = "3.18.0"
   #version = "4.0.0"
-  version = "5.1.0"      
+  version = "5.3.0"      
   
   #name = "private-sg"
   name = "${local.name}-private-sg"  
