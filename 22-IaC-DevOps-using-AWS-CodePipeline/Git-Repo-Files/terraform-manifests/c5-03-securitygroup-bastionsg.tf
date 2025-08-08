3cda48fb0557161260793e8894ecc4d7dd3ff49f
ashishdubey195 <68372389+ashishdubey195@users.noreply.github.com>
2025-08-08 11:01:33 +0100

    Update c5-03-securitygroup-bastionsg.tf

--- 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c5-03-securitygroup-bastionsg.tf
+++ 22-IaC-DevOps-using-AWS-CodePipeline/Git-Repo-Files/terraform-manifests/c5-03-securitygroup-bastionsg.tf
@@ -4,7 +4,7 @@ module "public_bastion_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   #version = "3.18.0"
   #version = "4.0.0"
-  version = "5.1.0"      
+  version = "5.3.0"      
   #name = "public-bastion-sg"  
   name = "${local.name}-public-bastion-sg"
   description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
